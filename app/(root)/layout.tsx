3e3063503742fd16a6ccb3f61e1722587e343416
weverton <weverton.wrb10@gmail.com>
2024-10-23 13:48:49 -0300

    fix: correct file import path

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -9,7 +9,19 @@ export default function RootLayout({
         <div className="bg-black-3 relative flex flex-col">
             <main className="relative flex bg-black-3">
                 <LeftSidebar />
-                {children}
+                <section>
+                  <div>
+                    <div>
+                      <Image  />
+                      MobileNav
+                    </div>
+                    <div>
+                      Toaster (notification popups)
+                      {children}
+                    </div>
+                  </div>
+                </section>
+                
                 <p className="text-white-1">RIGHT SIDEBAR</p>
             </main>
         </div>
