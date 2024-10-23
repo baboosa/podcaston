7f3be6cd91689fbc0f59c5f996ec70e770e8cead
weverton <weverton.wrb10@gmail.com>
2024-10-23 13:37:35 -0300

    feat: enhance sidebar links with active state styling

--- components/ui/LeftSidebar.tsx
+++ components/ui/LeftSidebar.tsx
@@ -1,9 +1,16 @@
+'use client'
+
 import Link from 'next/link'
 import Image from 'next/image'
 import React from 'react'
 import { sidebarLinks } from '@/constants'
+import { usePathname, useRouter } from 'next/navigation'
+import { cn } from '@/lib/utils'
 
 const LeftSidebar = () => {
+  const pathName = usePathname()
+  const router = useRouter()
+
   return (
     <section className='left_sidebar'>
       <nav className='flex flex-col gap-6'>
@@ -13,8 +20,11 @@ const LeftSidebar = () => {
         </Link>
 
         {sidebarLinks.map(({ route, label, imgURL }) => {
-          return <Link href={route} key={label} className='flex gap-3 items-center py-4 max-lg:px-4
-          justify-center lg:justify-start'>
+          const isActive = pathName === route || pathName.startsWith(`${route}/`)
+
+          return <Link href={route} key={label} className={
+            cn('flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start', {'bg-nav-focus border-r-4 border-orange-1': isActive})
+            }>
             <Image src={imgURL} alt={label} width={24} height={24} />
             <p>{label}</p>
           </Link>
