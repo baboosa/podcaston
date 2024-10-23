e05c8af379001c609fb5f6081a4afe2441753e0c
weverton <weverton.wrb10@gmail.com>
2024-10-23 11:23:42 -0300

    feat: add LeftSidebar component for improved UI layout

---
+++ components/ui/LeftSidebar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const LeftSidebar = () => {
+  return (
+    <section className='left_sidebar'>
+      <nav className='flex flex-col gap-6'>
+        <Link href='/'>
+          <Image />
+        </Link>
+      </nav>
+    </section>
+  )
+}
+
+export default LeftSidebar
