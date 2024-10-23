c7c4ebead0a68c8401b23109e5e8b7779f2181cf
weverton <weverton.wrb10@gmail.com>
2024-10-23 11:39:48 -0300

    feat: add logo to the pages

--- components/ui/LeftSidebar.tsx
+++ components/ui/LeftSidebar.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+import Image from 'next/image'
 import React from 'react'
 
 const LeftSidebar = () => {
@@ -5,7 +7,7 @@ const LeftSidebar = () => {
     <section className='left_sidebar'>
       <nav className='flex flex-col gap-6'>
         <Link href='/'>
-          <Image />
+          <Image src='/icons/logo.svg' alt='logo' width={23} height={27} />
         </Link>
       </nav>
     </section>
