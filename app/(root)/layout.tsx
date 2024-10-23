b4703d6539287dc76ccc389f97a2e2907797499d
weverton <weverton.wrb10@gmail.com>
2024-10-23 13:41:33 -0300

    style: add background and layout styles to main structure

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -6,8 +6,8 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <div className="bg-black-3">
-            <main>
+        <div className="bg-black-3 relative flex flex-col">
+            <main className="relative flex bg-black-3">
                 <LeftSidebar />
                 {children}
                 <p className="text-white-1">RIGHT SIDEBAR</p>
