81b02ddc7ec3e160220c05c571a6e68912d66d23
weverton <weverton.wrb10@gmail.com>
2024-10-23 10:16:20 -0300

    feat: Added trending podcasts section with updated styling. Using flexbox for responsive layout. Fixed title alignment issue.

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='mt-9 flex flex-col gap-9'>
+      <section className='flex flex-col gap-5'></section>
+      <h1 className='text-20 font-bold text-white-1'>Trending Podcasts</h1>
+    </div>
+  )
+}
+
+export default Home
