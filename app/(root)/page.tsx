4c0b40edb04349fd19246a3e866fb8c8311e8040
weverton <weverton.wrb10@gmail.com>
2024-10-23 15:03:38 -0300

    feat: add podcast display section with dynamic rendering

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,18 @@
+import PodcastCard from '@/components/PodcastCard'
+import { podcastData } from '@/constants'
 import React from 'react'
 
 const Home = () => {
   return (
     <div className='mt-9 flex flex-col gap-9'>
-      <section className='flex flex-col gap-5'></section>
-      <h1 className='text-20 font-bold text-white-1'>Trending Podcasts</h1>
+      <section className='flex flex-col gap-5'>
+        <h1 className='text-20 font-bold text-white-1'>Trending Podcasts</h1>
+        {podcastData.map(({ id, title, description, imgURL }) => (
+          <PodcastCard
+            key={id} imgUrl={imgURL} title={title} description={description} podcastId={id}
+          />
+        ))}
+      </section>
     </div>
   )
 }
