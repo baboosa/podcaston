61c5dda34e3e4a311215b12c185b1ff1f6b79fcd
weverton <weverton.wrb10@gmail.com>
2024-10-24 18:20:52 -0300

    feat: add authentication configuration for Clerk

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://large-barnacle-59.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+}
+
+export default authConfig
