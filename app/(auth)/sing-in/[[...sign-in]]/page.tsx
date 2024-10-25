b8b5a15727f2427c19a56a4dafdfbc335f0fffcd
weverton <weverton.wrb10@gmail.com>
2024-10-25 15:19:58 -0300

    feat: create structure for sign-up and sign-in pages

---
+++ app/(auth)/sing-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div className='flex-center glassmorphism-auth h-screen w-full'>
+      <SignIn />
+    </div>
+  )
+}
+
+export default Page
