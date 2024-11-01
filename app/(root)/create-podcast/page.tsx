ef4bfe121f88e7a366ef96f70bd8c2815b8f5afd
weverton <weverton.wrb10@gmail.com>
2024-11-01 12:07:48 -0300

    feat: enhance podcast creation form with voice selection and description

--- app/(root)/create-podcast/page.tsx
+++ app/(root)/create-podcast/page.tsx
@@ -16,6 +16,20 @@ import {
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { cn } from "@/lib/utils"
+import { useState } from "react"
+import { Textarea } from "@/components/ui/textarea"
+import GenerateThumbnail from "@/components/GenerateThumbnail"
+import GeneratePodcast from "@/components/GeneratePodcast"
+
+const voiceCategories = ['alloy', 'shimmer', 'nova', 'echo', 'fable', 'onyx']
 
 const formSchema = z.object({
   username: z.string().min(2, {
@@ -24,6 +38,8 @@ const formSchema = z.object({
 })
 
 const CreatePodcast = () => {
+  const [voiceType, setVoiceType] = useState<string | null>(null)
+
   // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -64,9 +80,48 @@ const CreatePodcast = () => {
               <Label className="text-16 text-bold text-white-1">
                 Select AI Voice
               </Label>
-            </div>
-          </div>
 
+              <Select onValueChange={(value) => setVoiceType(value)}>
+                <SelectTrigger className={cn('text-16 w-full border-none bg-black-1 text-gray-1')}>
+                  <SelectValue placeholder="Select AI Voice" className="placeholder:text-gray-1" />
+                </SelectTrigger>
+                <SelectContent className="text-16 border-none bg-black-1 font-bold text-white-1 focus:ring-orange-1">
+                  {voiceCategories.map((category) => (
+                    <SelectItem key={category} value={category} className="capitalize focus:bg-orange-1">
+                      {category}
+                    </SelectItem>
+                  ))
+                  }
+                </SelectContent>
+
+                {voiceType && (
+                  <audio src={`/${voiceType}.mp3`}
+                    autoPlay
+                    className="hidden">
+                  </audio>
+                )}
+              </Select>
+            </div>
+
+            <FormField
+              control={form.control}
+              name="podcastDescription"
+              render={({ field }) => (
+                <FormItem className="flex flex-col gap-2.5">
+                  <FormLabel className="text-16 text-white-1 font-bold">Description</FormLabel>
+                  <FormControl>
+                    <Textarea className="input-class focus-visible:ring-orange-1" placeholder="Write a short podcast" {...field} />
+                  </FormControl>
+                  <FormMessage className="text-white-1" />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="flex flex-col pt-10">
+            <GeneratePodcast />
+
+            <GenerateThumbnail />
+          </div>
           <Button type="submit">Submit</Button>
         </form>
       </Form>
